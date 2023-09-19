2e8524dac272581b596efd19353cf21add9210a9
Eduardo Santos <eduardo.wosgrau@gmail.com>
2023-09-19 11:25:49 -0300

    reactive courses list, lab ref(), format

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive } from "vue";
+
+// ref for primitives (number, string, etc)
+let count = ref(0);
+
+// reactive for objects
+let courses = reactive([
+  {
+    title: "Javascript",
+  },
+  {
+    title: "React",
+  },
+  {
+    title: "Vue",
+  },
+]);
+
+let newCourse = reactive({});
+
+// .value for ref()
+function increment() {
+  count.value++;
+}
+
+function addCourse() {
+  courses.push(newCourse);
+  newCourse = {};
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Courses List</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.title">
+        {{ course.title }}
+      </li>
+    </ul>
+    <br />
+    <input v-model="newCourse.title" type="text" />
+    <button @click="addCourse">Add</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
